39cfbe7cb17eb7f4d4751030c7285e7da7b97cbd
MilimetrosDeMercurio <00225117@uca.edu.sv>
2019-10-30 11:40:49 -0600

    Updates

---
+++ precache-manifest.b0cc7eeac313584f3da7753058528ceb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17e5a7094bef5c9d2d1bb4df62c595b",
+    "url": "MilimetrosDeMercurio.github.io/tictactoe/index.html"
+  },
+  {
+    "revision": "9492b61b83d3c57901a2",
+    "url": "MilimetrosDeMercurio.github.io/tictactoe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "5fc33aed0e7becc2a37c",
+    "url": "MilimetrosDeMercurio.github.io/tictactoe/static/js/2.52dd166e.chunk.js"
+  },
+  {
+    "revision": "9492b61b83d3c57901a2",
+    "url": "MilimetrosDeMercurio.github.io/tictactoe/static/js/main.d9f8ea86.chunk.js"
+  },
+  {
+    "revision": "3a4ce9c1cecada87c1ac",
+    "url": "MilimetrosDeMercurio.github.io/tictactoe/static/js/runtime-main.d34c9161.js"
+  }
+]);
